b1f85606843a437d375713ccfb503292f7cde146
Darshan.V <darshanv487@gmail.com>
2024-12-24 12:18:47 +0530

    CodeWithHarry/chapter1/module.py: learnt about what is modules, pip and comments

--- CodeWithHarry/chapter1/module.py
+++ CodeWithHarry/chapter1/module.py
@@ -1,4 +1,8 @@
 import pyjokes
-
+# print("printing jokes...")
 jokes=pyjokes.get_joke()
 print(jokes)
+
+
+# if we want to use a code which is already built in our app then we will use modules , we will install modules using pip 
+# if we want to print something and that shouldn't be printed then we will use comments
