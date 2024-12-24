f00647fe5118c72f3f4f45edcc115ca9cbc0d33f
Darshan.V <darshanv487@gmail.com>
2024-12-24 12:31:34 +0530

    CodeWithHarry/chapter1/module.py: learnt about multi line comments

--- CodeWithHarry/chapter1/module.py
+++ CodeWithHarry/chapter1/module.py
@@ -3,6 +3,10 @@ import pyjokes
 jokes=pyjokes.get_joke()
 print(jokes)
 
+"""
+this is a multiline comment
+I am learning python
+"""
 
 # if we want to use a code which is already built in our app then we will use modules , we will install modules using pip 
 # if we want to print something and that shouldn't be printed then we will use comments
