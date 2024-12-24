b5619e68a700e3482109673262348c53393b9ecc
Darshan.V <darshanv487@gmail.com>
2024-12-24 14:11:39 +0530

    CodeWithHarry/chapter2/datatypes.py: learnt about the basic datatypes which are there in the python

---
+++ CodeWithHarry/chapter2/datatypes.py
@@ -0,0 +1,14 @@
+# in python there are 5 types of datatypes 
+
+#  String
+# floating point numbers
+# integer
+# boolean
+# None
+
+
+a="hello" # string
+b=1.0 # floating point number
+c=1 # integer
+d=True # boolean
+e=None # None
