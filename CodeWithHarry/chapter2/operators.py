de224ffe05d6ee47b7eb993fa07952f99b4a6d39
Darshan.V <darshanv487@gmail.com>
2024-12-26 12:30:00 +0530

    CodeWithHarry/chapter2/operators.py : learnt about the operators in python

---
+++ CodeWithHarry/chapter2/operators.py
@@ -0,0 +1,43 @@
+# OPERATORS IN PYTHON
+# Following are some common operators in python:
+# 1.
+# Arithmetic operators: +, -, *, / etc.
+# 2.
+# Assignment operators: =, +=, -= etc.
+# 3.
+# Comparison operators: ==, >, >=, <, != etc.
+# 4.
+# Logical operators: and, or, not.
+
+
+# Arithmetic operators
+a=7
+b=4
+c=a+b
+print(c)
+
+# Assignment operators:
+
+a=4-2
+print(a)
+b=6
+# b+=3
+b-=3
+print(b)
+
+#comparison operators
+d=5
+e=6
+print(d==e)
+print(d>e)
+print(d>=e)
+print(d<=e)
+print(d!=e)
+
+#Logocal operators
+print("logical operators")
+a=True
+b=False
+print(a and b)
+print(a or b)
+print(not a)
