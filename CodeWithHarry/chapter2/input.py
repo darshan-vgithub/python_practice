dede31635314670a539c935a01a5a37c5960a21a
Darshan.V <darshanv487@gmail.com>
2024-12-26 20:47:31 +0530

    CodeWithHarry/chapter2/input.py: learnt the detailed code of input

--- CodeWithHarry/chapter2/input.py
+++ CodeWithHarry/chapter2/input.py
@@ -1,5 +1,6 @@
-a=input("Enter the number 1: ")
-b=input("Enter the number 2: ")
+a=int(input("Enter the number 1: "))
+b=int(input("Enter the number 2: "))
 
 print("Number a is :",a)
 print("Number b is :",b)
+print("Sum of a and b is:",a+b)
