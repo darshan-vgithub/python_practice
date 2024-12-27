f2f4bc84a51e7bc17e744ff990440866b0228faf
Darshan.V <darshanv487@gmail.com>
2024-12-27 11:55:04 +0530

    CodeWithHarry/chapter2/parcticeSet/: done with the chapter 2 practice sets

---
+++ CodeWithHarry/chapter2/parcticeSet/pb5.py
@@ -0,0 +1,5 @@
+a=int(input("Enter the number 1: "))
+b=int(input("Enter the number 2: "))
+
+avgoftwonums=(a+b)/2
+print(avgoftwonums)
---
+++ CodeWithHarry/chapter2/parcticeSet/pb6.py
@@ -0,0 +1,3 @@
+a=int(input("Enter the number 1: "))
+findSquareOfNum=a*a
+print(findSquareOfNum)
