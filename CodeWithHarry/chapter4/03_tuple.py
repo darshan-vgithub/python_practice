6a097361ba0861280c1ec0135471a83233282a1a
Darshan.V <darshanv487@gmail.com>
2025-01-01 13:20:14 +0530

    CodeWithHarry/chapter4/03_tuple.py: learnt the basics of tuple

---
+++ CodeWithHarry/chapter4/03_tuple.py
@@ -0,0 +1,18 @@
+a=(1,2,3,4,5)
+print(a)
+print(type(a))
+
+b=(1)
+print(b)
+print(type(b)) # this is will be a type of int 
+
+
+# if we want a tuple and in that tuple we want only one element then we have to put comma next to that element
+
+c=(1,)
+print(c)
+print(type(c))
+
+hetero=(1,2,3,4,5,"Darshan",True,1.0,3+4j)
+print(hetero)
+print(type(hetero))
