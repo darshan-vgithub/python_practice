5c8f4a2b33a84bbb14556a5bbd20c9c1da05a4c5
Darshan.V <darshanv487@gmail.com>
2025-01-01 18:57:42 +0530

    CodeWithHarry/chapter5/dict.py: learnt the basics of the dictionary

---
+++ CodeWithHarry/chapter5/dict.py
@@ -0,0 +1,18 @@
+marks={
+    "darshan":100,
+    "dudu":90,
+    "bubu":80,
+    "manish":70,
+    "shubham":60,
+    "rohit":50
+}
+
+print(marks,type(marks))
+"""
+dict is mutable
+it is unordered
+it is indexed
+cannot contain duplicate
+"""
+
+print(marks["dudu"])
