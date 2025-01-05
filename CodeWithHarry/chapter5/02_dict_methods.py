189c660dfcc774819bda87c1a4eafc7deef3bd3b
Darshan.V <darshanv487@gmail.com>
2025-01-05 11:52:09 +0530

    CodeWithHarry/chapter5/02_dict_methods.py: learnt some methids in dictionary

---
+++ CodeWithHarry/chapter5/02_dict_methods.py
@@ -0,0 +1,33 @@
+marks={
+    "darshan":100,
+    "dudu":90,
+    "bubu":80,
+    "manish":70,
+    "shubham":60,
+    "rohit":50
+}
+
+print(marks.items())
+print(marks.keys())
+print(marks.values())
+marks.update({"bubu":100})
+print(marks)
+
+marks.update({"bubu":100,"lala":300})
+print(marks)
+
+marks.get("shivika")
+
+# clear()
+# copy()
+# fromkeys(iterable, value=None)
+# get(key, default=None)
+# items()
+# keys()
+# pop(key, default=None)
+# popitem()
+# setdefault(key, default=None)
+# update([other])
+# values()
+
+# some of the other methods which i want to try it out
