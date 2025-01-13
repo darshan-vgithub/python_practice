c8133f02d35fe45cc63511f2c38002ee6e0b5a8b
Darshan.V <darshanv487@gmail.com>
2025-01-13 21:33:00 +0530

    CodeWithHarry/chapter5/03_set.py: learnt the basics of sets

---
+++ CodeWithHarry/chapter5/03_set.py
@@ -0,0 +1,12 @@
+s={1,2,3}
+print(s)
+
+print(type(s))
+
+emptyset=set() # if  to create a setyou want to create an empty set then you have to craete like this , dont use {}
+print(emptyset)
+print(type(emptyset))
+
+# in set elements cannot be duplicated
+s={1,2,3,1,2,3,4,5,6,7,8,9,10} # it will print only unique elements
+print(s)
