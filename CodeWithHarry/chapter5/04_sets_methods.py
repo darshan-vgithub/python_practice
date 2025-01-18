fb206ac8ec64891930219f25036c24692db5bd02
Darshan.V <darshanv487@gmail.com>
2025-01-18 11:31:14 +0530

    CodeWithHarry/chapter5/04_sets_methods.py: learnt the bascis of sets

---
+++ CodeWithHarry/chapter5/04_sets_methods.py
@@ -0,0 +1,22 @@
+# Set is a collection of non-repetitive elements.
+
+# 1.
+# Sets are unordered => Element’s order doesn’t matter
+# 2.
+# Sets are unindexed => Cannot access elements by index
+# 3.
+# There is no way to change items in sets.
+# 4.
+# Sets cannot contain duplicate values.
+s={1,2,3,4}
+s.add(23)
+print(s, type(s))
+
+# operations 
+
+s1={1,2,3,4}
+print(len(s1))
+
+
+print(s1.remove(3))
+print(s1)
