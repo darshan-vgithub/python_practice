8dc76fea8a338742e935d27e1849e7f18eb776a0
Darshan.V <darshanv487@gmail.com>
2025-02-03 16:16:18 +0530

    CodeWithHarry/chapter6/02_if_elif_else_laddar.py: learnt about elif

---
+++ CodeWithHarry/chapter6/02_if_elif_else_laddar.py
@@ -0,0 +1,21 @@
+age=int(input("Enter your age: "))
+
+if(age>=18):
+    print("You are eligible to vote")
+
+elif(age<0):
+    print("Invalid age")
+
+elif(age==0):
+    print("You are a baby")
+
+elif(age==1):
+    print("You are a toddler")
+
+elif(age==2):
+    print("You are a toddler")
+
+elif(age==3):
+    print("You are a toddler")
+else:
+    print("You are not eligible to vote")
