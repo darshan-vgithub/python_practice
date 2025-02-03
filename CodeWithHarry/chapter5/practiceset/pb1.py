f09706d1d6f48d99d752fca25bed288991a5aca4
Darshan.V <darshanv487@gmail.com>
2025-02-03 13:09:28 +0530

    CodeWithHarry/chapter5/practiceset/pb3.py: doing the practice set of dict and set

---
+++ CodeWithHarry/chapter5/practiceset/pb1.py
@@ -0,0 +1,16 @@
+words={
+    "madad":"Help",
+    "Aam":"Mango",
+    "Kursi":"Chair",
+    "Dabba":"Box",
+    "Pehla":"First",
+    "Dusra":"Second",
+    "Tere":"Your",
+    "Pyar":"Love",
+    "Bhi":"Also"
+}
+
+
+word=input("Enter the word you want to search: ")
+
+print(words[word])
