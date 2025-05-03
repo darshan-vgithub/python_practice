431cb7e58fa3133b85fa9afb25eecb5882065f7c
Darshan.V <darshanv487@gmail.com>
2025-05-03 18:59:55 +0530

    saw the os module

--- CodeWithHarryPython/chapter1/Chapter1PracticeSet/pb4.py
+++ CodeWithHarryPython/chapter1/Chapter1PracticeSet/pb4.py
@@ -1,2 +1,15 @@
-from emoji import emojize
-print(emojize(":thumbs_up:"))
+# Write a python program to print the contents of a directory using the os module. Search online for the function which does that.
+import os
+
+# Specify the directory you want to list
+directory_path = '/'  # current directory, you can change it to any path
+
+try:
+    contents = os.listdir(directory_path)
+    print(f"Contents of directory '{directory_path}':")
+    for item in contents:
+        print(item)
+except FileNotFoundError:
+    print(f"The directory '{directory_path}' does not exist.")
+except PermissionError:
+    print(f"Permission denied to access '{directory_path}'.")
