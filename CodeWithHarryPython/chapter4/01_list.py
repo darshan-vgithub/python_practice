5b1f30390241a3625a641baf9830cda9fb577619
Darshan.V <darshanv487@gmail.com>
2025-05-05 12:37:44 +0530

    CodeWithHarryPython/chapter4/01_list.py: learnt basis about the list structure and also learnt about indexing and list slicing

---
+++ CodeWithHarryPython/chapter4/01_list.py
@@ -0,0 +1,10 @@
+# A list in Python is an ordered, mutable (changeable) collection of items. Lists can contain elements of any data type, and items can be repeated.
+friends=["Apple","Banana","Cherry",1,12.4,True]
+print(friends)
+print(friends[0])
+print(friends[-1])
+print(friends[0:2]) # starts from index 0 and ends at index 
+print(friends[0])
+friends[0]="Peach"
+print(friends[0])
+#  list are mutable unlike strings
