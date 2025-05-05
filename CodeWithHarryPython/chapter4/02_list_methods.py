3d61075ceaf75bf581313c6e0f57062a14b4e5ef
Darshan.V <darshanv487@gmail.com>
2025-05-05 13:07:53 +0530

    CodeWithHarryPython/chapter4/02_list_methods.py: learnt some list methods

---
+++ CodeWithHarryPython/chapter4/02_list_methods.py
@@ -0,0 +1,13 @@
+friends=["Apple","Banana","Cherry",1,12.4,True]
+print(friends)
+
+friends.append("Rohan")
+print(friends)
+
+l1= [1,8,7,2,21,15]
+l1.sort()
+l1.reverse()
+l1.insert(2,33333)
+l1.pop() # it will remove the last element from the list and also if we give index it will remove the element at that index
+print(l1)
+
