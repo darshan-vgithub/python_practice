145ee82261234390812361fad140408d83ea7e4a
Darshan.V <darshanv487@gmail.com>
2025-05-15 12:35:42 +0530

    CodeWithHarryPython/chapter7/05_for_loop_iterate.py: modified

--- CodeWithHarryPython/chapter7/05_for_loop_iterate.py
+++ CodeWithHarryPython/chapter7/05_for_loop_iterate.py
@@ -14,3 +14,13 @@ d={"a":1,"b":2,"c":3,"d":4}
 for i in d:
     print(f"{i}:{d[i]}")
 
+
+
+# factorial of a number
+
+# n=int(input("Enter the number: "))
+# factorial=1
+# for i in range(1,n+1):
+#     factorial=factorial*i
+# print(factorial) just trying out the factorial problem
+
