fd791c1fe359ed69b9602d68d5cb6d6ef1017dc8
Darshan.V <darshanv487@gmail.com>
2025-05-21 17:32:23 +0530

    built a function where to read a specific word from a file

---
+++ CodeWithHarryPython/chapter9/PracticeSet/pb1.py
@@ -0,0 +1,14 @@
+# Write a program to read the text from a given file ‘poems.txt’ and find out whether it contains the word ‘twinkle’.
+import os
+def find_the_word_from_file():
+    base_path=os.path.dirname(os.path.abspath(__file__))
+    file_path=os.path.join(base_path,"poem.txt")
+    f=open(file_path)
+    data=f.read()
+    if "twinkle" in data:
+        print("Twinkle is present in the file")
+    else:
+        print("Twinkle is not present in the file")
+    f.close()
+
+find_the_word_from_file()
