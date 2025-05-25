76153f16fd33e168d9506679dd3228c29637d16d
Darshan.V <darshanv487@gmail.com>
2025-05-25 17:58:27 +0530

    started with OOPS

---
+++ CodeWithHarryPython/chapter10/01_class.py
@@ -0,0 +1,14 @@
+class Employee:
+    name="Darshan"
+    age=25
+    language="Python"
+    salary=100000
+
+Darshan=Employee()
+print(Darshan.name)
+print(Darshan.age)
+print(Darshan.language)
+print(Darshan.salary)  # this is a object 
+
+
+# class is a blueprint
