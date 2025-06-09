63ae02e6955920e1e3b36daa76e0907aed848fe6
Darshan.V <darshanv487@gmail.com>
2025-06-09 16:22:15 +0530

    CodeWithHarryPython/chapter11/04_super.py: modified

--- CodeWithHarryPython/chapter11/04_super.py
+++ CodeWithHarryPython/chapter11/04_super.py
@@ -1,9 +1,9 @@
 class Employee:
     def __init__(self):
-        print("this is a constructor")
+        print("this is a constructor of Employee")
 class Programmer(Employee):
     def __init__(self):
-        # super().__init__()
+        super().__init__()
         print("this is a constructor of Programmer")
 
 p=Programmer()
